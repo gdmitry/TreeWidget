0b6aa0584d3626652d9a1f014e5ce945075d86a9
Dmitry Goncharenko <dmitry.goncharenko24@gmail.com>
2017-06-13 01:44:50 +0300

    modify

--- js/Node.js
+++ js/Node.js
@@ -19,6 +19,7 @@ export class Node {
 
         let nameEl = document.createElement('div');
         nameEl.className = "name";
+        nameEl.textContent = this.name;
         nodeEl.appendChild(nameEl);
 
         if (this.hasChildren) {
